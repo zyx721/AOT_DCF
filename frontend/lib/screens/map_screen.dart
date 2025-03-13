d0b8609ad0f8203154773b864f72ed5809082325
benmati ziad <152486493+zyx721@users.noreply.github.com>
2025-03-13 13:52:22 +0100

    added maps

---
+++ frontend/lib/screens/map_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Volunteer Map")),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(36.7528, 3.0422), // Example: Algiers
+          zoom: 13.0,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: const ['a', 'b', 'c'],
+          ),
+        ],
+      ),
+    );
+  }
+}
